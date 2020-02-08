57bf4673a23e013264e195a6415350da96e8cfb6
wengerari <wengerari@gmail.com>
2020-02-09 00:34:56 +0300

    Updates

---
+++ precache-manifest.edde119cddddad0f8a7d2b840553151f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cba877b8e122433a31ae81eb9af14ed",
+    "url": "/onlineShop/index.html"
+  },
+  {
+    "revision": "572082fe6ce548a2819e",
+    "url": "/onlineShop/static/css/main.c55d6082.chunk.css"
+  },
+  {
+    "revision": "1c16469549c009a67af7",
+    "url": "/onlineShop/static/js/2.811f0841.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/onlineShop/static/js/2.811f0841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572082fe6ce548a2819e",
+    "url": "/onlineShop/static/js/main.96024c85.chunk.js"
+  },
+  {
+    "revision": "6e6157f2d6af0148eea3",
+    "url": "/onlineShop/static/js/runtime-main.e810dfe3.js"
+  },
+  {
+    "revision": "43f50358a3cb4f51c4e3b4c77ab5adff",
+    "url": "/onlineShop/static/media/animal-paw-print.43f50358.svg"
+  },
+  {
+    "revision": "2c541ed1f9082b9fc5ed22cafe95ba72",
+    "url": "/onlineShop/static/media/shopping-store.2c541ed1.svg"
+  }
+]);
